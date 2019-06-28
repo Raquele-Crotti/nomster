6c1aa4b2bdbd594fc29edab7fdd92e154c6ed8ad
Raquele Crotti <raquelecrotti@gmail.com>
2019-06-28 08:23:21 +0000

    Added app/views/places/new.html.erb file with a heading saying Enter Your Place Details

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,10 +1,10 @@
 class PlacesController < ApplicationController
   
   def index
-    @places = Place.order("name").page(params[:page]).per(10)
+    @places = Place.order("name").page(params[:page])
   end
 
-  def show
+  def new
     
   end
 
