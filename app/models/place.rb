374be65112d757a6dd845e5c988859fba0c80275
Raquele Crotti <raquelecrotti@gmail.com>
2019-07-08 08:21:00 +0000

    Set up connections for photos

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,8 +1,11 @@
 class Place < ApplicationRecord
   belongs_to :user
   has_many :comments
+  has_many :photos
+
   geocoded_by :address
   after_validation :geocode
+  
   validates :name, presence: true, length: {minimum: 3}
   validates :address, presence: true
   validates :description, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_04_064038) do
+ActiveRecord::Schema.define(version: 2019_07_08_080558) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,16 @@ ActiveRecord::Schema.define(version: 2019_07_04_064038) do
     t.datetime "updated_at", null: false
     t.index ["place_id"], name: "index_comments_on_place_id"
     t.index ["user_id", "place_id"], name: "index_comments_on_user_id_and_place_id"
+  end
+
+  create_table "photos", force: :cascade do |t|
+    t.text "caption"
+    t.integer "user_id"
+    t.integer "place_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["place_id"], name: "index_photos_on_place_id"
+    t.index ["user_id", "place_id"], name: "index_photos_on_user_id_and_place_id"
   end
 
   create_table "places", force: :cascade do |t|
