f250952327b85ca7f7460222371c43f07d495332
Raquele Crotti <raquelecrotti@gmail.com>
2019-07-20 06:28:12 +0000

    Adjusted config/environments/development.rb and config/application.yml to have email and passsword set up to send automated emails to people who received a comment on the place they created

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,16 @@
 Rails.application.configure do
+
+    config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "example.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV['GMAIL_ADDRESS'],
+    password: ENV['GMAIL_PASSWORD']
+  }
   config.action_mailer.default_url_options= { host: 'localhost:3030'}
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -33,7 +45,7 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
