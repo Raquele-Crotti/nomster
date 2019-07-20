268aa2316916070b424dde04279076f8d3cbd272
Raquele Crotti <raquelecrotti@gmail.com>
2019-07-20 07:55:13 +0000

    Set up comment on places notification on heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,15 @@
 Rails.application.configure do
+
+   ActionMailer::Base.smtp_settings = {
+    address: 'smtp.sendgrid.net',
+    port: '587',
+    authentication: :plain,
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    domain: 'heroku.com',
+    enable_starttls_auto: true
+  }
+
   config.action_mailer.default_url_options = { host: 'nomster-raquele-crotti.herokuapp.com'}
   # Settings specified here will take precedence over those in config/application.rb.
 
