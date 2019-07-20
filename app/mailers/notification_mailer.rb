ec41f90167017089091073a7af1fb25808495ef3
Raquele Crotti <raquelecrotti@gmail.com>
2019-07-20 07:11:40 +0000

    Adjusted automated email mailer for users who created a place and receive a comment on their place. Added a link to get to the place page on the email.

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,9 +1,12 @@
 class NotificationMailer < ApplicationMailer
   default from: "no-reply@nomsterapp.com"
 
-  def comment_added
-    mail(to: "raquelecrotti@gmail.com",
-      subject: "A comment has been added to your place")
+  def comment_added(comment)
+    @place = comment.place    #to get to the place the comment was made for
+    @place_owner = @place.user #to get to the user who created the place
+
+    mail(to: @place_owner.email,  #to send email to place owner's email address
+      subject: "A comment has been added to #{@place.name}")
   end
 
 end
